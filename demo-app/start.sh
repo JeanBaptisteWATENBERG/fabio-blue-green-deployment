2361765278189110c9e6297ce7abddf6b940778a
Jean-Baptiste WATENBERG <jeanbaptiste.watenberg@gmail.com>
2018-11-29 00:07:53 +0100

    signal is trapped

--- demo-app/start.sh
+++ demo-app/start.sh
@@ -10,17 +10,19 @@ pid=0
 term_handler() {
   if [ $pid -ne 0 ]; then
     curl -X PUT http://$CONSUL:8500/v1/agent/service/deregister/python-test-${HOSTNAME} -v
+    sleep 1 # wait for last requests to be processed - should definitely find a better way, indeed if treatment is longer it will still end up with 502 in extrem cases
+    # we should tail access log and applicative logs and watch if something is happenning on the container, then wait for last access log entry
     kill -SIGTERM "$pid"
     wait "$pid"
   fi
   exit 143; # 128 + 15 -- SIGTERM
 }
 
-trap term_handler 15
+trap "kill ${!}; term_handler" 15
 python3 -m http.server --bind 0.0.0.0 --cgi 8000 &> app.log &
 pid="$!"
 
-# tail -F app.log
+#tail -F app.log
 # wait forever
 while true
 do
